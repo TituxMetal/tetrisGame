d651b6939f04a2f54023141c82f17ff57b564e16
Laptop | Lang Guillaume <lang.guillaume@gmail.com>
2018-06-28 11:42:05 +0200

    Update Player class; add the collision detection when player move/drop

--- src/Player.js
+++ src/Player.js
@@ -1,25 +1,34 @@
 class Player {
-  
-  constructor() {
+
+  constructor(tetris) {
     this.dropCounter = 0
     this.dropInterval = 900
     this.position = { x: 0, y: 0 }
     this.matrix = null
+    this.arena = tetris.arena
     this.score = 0
   }
 
   drop() {
     this.position.y++
     this.dropCounter = 0
+
+    if (this.arena.collide(this)) {
+      this.position.y--
+    }
   }
 
   move(direction) {
     this.position.x += direction
+
+    if (this.arena.collide(this)) {
+      this.position.x -= direction
+    }
   }
 
   update(deltaTime) {
     this.dropCounter += deltaTime
-    
+
     if (this.dropCounter > this.dropInterval) {
       this.drop()
     }
