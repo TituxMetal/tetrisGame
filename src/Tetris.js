f82c3867899b7c51af81df0080aec6f495b6e59b
Laptop | Lang Guillaume <lang.guillaume@gmail.com>
2018-06-28 08:18:53 +0200

    Add the Tetris class

---
+++ src/Tetris.js
@@ -0,0 +1,58 @@
+class Tetris {
+  constructor(canvas, player) {
+    this.canvas = canvas
+    this.context = canvas.getContext('2d')
+    this.scale = 16
+    this.lastTime = 0
+    this.requestAnimationID = undefined
+    this.player = player
+
+    this.context.scale(this.scale, this.scale)
+    this.start()
+  }
+
+  draw() {
+    this.context.fillStyle = '#444'
+    this.context.fillRect(0, 0, this.canvas.width, this.canvas.height)
+    this.drawMatrix(this.player.matrix, this.player.position)
+  }
+
+  drawMatrix(matrix, offset) {
+    matrix.forEach((row, y) => {
+      row.forEach((value, x) => {
+        if (value !== 0) {
+          this.context.fillStyle = 'violet'
+          this.context.fillRect(x + offset.x, y + offset.y, 1, 1)
+        }
+      })
+    })
+  }
+
+  pause() {
+    if (this.requestAnimationID) {
+      cancelAnimationFrame(this.requestAnimationID)
+      this.requestAnimationID = undefined
+      return
+    }
+
+    this.start()
+  }
+
+  start() {
+    if (this.requestAnimationID === undefined) {
+      this.requestAnimationID = requestAnimationFrame(this.update.bind(this))
+    }
+  }
+
+  update(time = 0) {
+    const deltaTime = time - this.lastTime
+    this.lastTime = time
+
+    this.player.update(deltaTime)
+    this.draw()
+
+    this.requestAnimationID = requestAnimationFrame(this.update.bind(this))
+  }
+}
+
+export default Tetris
