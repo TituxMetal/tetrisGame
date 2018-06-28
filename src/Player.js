810372e232780546c8f32d9c615cdf5854c84821
Laptop | Lang Guillaume <lang.guillaume@gmail.com>
2018-06-28 11:56:45 +0200

    Update Player class; add rotate and rotateMatrix methods that allows the player to rotate

--- src/Player.js
+++ src/Player.js
@@ -26,6 +26,43 @@ class Player {
     }
   }
 
+  rotate(dir) {
+    const pos = this.position.x
+    let offset = 1
+    this.rotateMatrix(dir)
+
+    while (this.arena.collide(this)) {
+      this.position.x += offset
+      offset = -(offset + (offset > 0 ? 1 : -1))
+      
+      if (offset > this.matrix[0].length) {
+        this.rotateMatrix(-dir)
+        this.position.x = pos
+        return
+      }
+    }
+  }
+
+  rotateMatrix(dir) {
+    for (let y = 0; y < this.matrix.length; ++y) {
+      for (let x = 0; x < y; ++x) {
+        [
+          this.matrix[x][y],
+          this.matrix[y][x]
+        ] = [
+          this.matrix[y][x],
+          this.matrix[x][y]
+        ]
+      }
+    }
+
+    if (dir > 0) {
+      return this.matrix.forEach(row => row.reverse())
+    }
+
+    return this.matrix.reverse()
+  }
+
   update(deltaTime) {
     this.dropCounter += deltaTime
 
