86bce6de0cc7ce7ec633bbb6c0bdccee202ffee9
Laptop | Lang Guillaume <lang.guillaume@gmail.com>
2018-06-28 11:39:07 +0200

    Update Arena class; Add collide method to manage collisions

--- src/Arena.js
+++ src/Arena.js
@@ -8,6 +8,22 @@ class Arena {
 
     this.matrix = matrix
   }
+
+  collide(player) {
+    const [matrix, offset] = [player.matrix, player.position]
+
+    for (let y = 0; y < matrix.length; ++y) {
+      for (let x = 0; x < matrix[y].length; ++x) {
+        if (matrix[y][x] !== 0 &&
+          (this.matrix[y + offset.y] &&
+            this.matrix[y + offset.y][x + offset.x]) !== 0) {
+          return true
+        }
+      }
+    }
+
+    return false
+  }
 }
 
 export default Arena
