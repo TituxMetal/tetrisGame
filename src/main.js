7f02a88215d007b053753094f63a5dd5f4565371
Laptop | Lang Guillaume <lang.guillaume@gmail.com>
2018-07-02 16:44:11 +0200

    Add main.js file for server side

---
+++ src/main.js
@@ -0,0 +1,40 @@
+const WebSocketServer = require('ws').Server
+
+const ConnectionManager = require('./ConnectionManager')
+
+const server = new WebSocketServer({ port: 9000 })
+const connectionManager = new ConnectionManager()
+
+server.on('connection', connection => {
+  const client = connectionManager.initClient(connection)
+
+  console.log('Connection established and new client created')
+
+  connection.on('message', message => {
+    const data = JSON.parse(message)
+    const messageType = data.type
+
+    console.log(`Message received`)
+
+    switch (messageType) {
+      case ('initSession'):
+        connectionManager.initSession(client)
+        console.log('initSession Ok')
+        break
+      case ('joinSession'):
+        connectionManager.joinSession(client, data.id)
+        console.log('joinSession Ok')
+        break
+    }
+  })
+
+  connection.on('close', () => {
+    connectionManager.disconnect(client)
+    
+    const sessionCount = connectionManager.sessions.size
+
+    console.log(
+      `Client disconnected,there is now ${sessionCount} session${sessionCount > 1 ? 's' : ''}`
+    )
+  })
+})
